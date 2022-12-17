d55058a82beae072c94e9098a93580f57b626b2a
Hall-Of-Fame-03 <111757562+Hall-Of-Fame-03@users.noreply.github.com>
2022-12-17 10:39:32 +0530

    Add files via upload

---
+++ src/src/data.js
@@ -0,0 +1,68 @@
+import beach1 from "./images/images.png";
+import beach2 from "./images/images.png";
+import beach3 from "./images/images.png";
+import desert1 from "./images/images.png";
+import desert2 from "./images/images.png";
+import desert3 from "./images/images.png";
+import mountain1 from "./images/images.png";
+import mountain2 from "./images/images.png";
+
+export const data = [
+  {
+    id: 1,
+    img: mountain2,
+    category: "Quiz",
+    title: "Quiz",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 2,
+    img: desert3,
+    category: "Hackathon",
+    title: "Hackathon",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 3,
+    img: beach2,
+    category: "Research",
+    title: "Research paper",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 4,
+    img: mountain1,
+    category: "Scholarships",
+    title: "Scholarships",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 5,
+    img: desert2,
+    category: "Coding Contest",
+    title: "Coding Contest",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 6,
+    img: desert1,
+    category: "Internships",
+    title: "Internships",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 7,
+    img: beach3,
+    category: "Technical Programs",
+    title: "Technical Programs",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  {
+    id: 8,
+    img: beach1,
+    category: "Speaker @ Conference",
+    title: "Speaker @ Conference",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, saepe!",
+  },
+  
+];
